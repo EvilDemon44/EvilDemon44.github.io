ce8763b0efbd669d3dd3cdb49b4fb1253dccb2f3
EvilDemon44 <alexanderkburns2318@gmail.com>
2025-01-22 00:33:27 -0700

    buttons

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+
+
+
+
+function DarkMode() {
+    document.getElementById('PageBody').style="background-color: #363635;";
+
+
+}
+
+function LightMode() {
+    document.getElementById('PageBody').style="background-color: #FFFFFF;";
+    document.getElementsByClassName('dark').style="color #FFFFFF;";
+    document.getElementsByClassName('light').style.color="#FFFFFF;";
+
+
+}
+function Default() {
+    document.getElementById('PageBody').style="background-color: #;";
+
+
+}
