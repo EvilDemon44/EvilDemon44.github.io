38b5b54a238b99e8b23c606a0160959282d13a0b
EvilDemon44 <alexanderkburns2318@gmail.com>
2025-01-22 13:54:35 -0700

    working on buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,28 +4,38 @@
 
 function DarkMode() {
     document.getElementById('PageBody').style="background-color: #363635;";
-    document.getElementById('head1').style="color: #FFFFFF;";
-    document.getElementById('head2').style="color: #FFFFFF;";
-    document.getElementById('para1').style="color: #B39C4D;";
-    document.getElementById('para2').style="color: #B39C4D;";
-    document.getElementById('footer').style="color: #EAE0D5;";
+    document.getElementById('head1').style="color: #FFBF00;";
+    document.getElementById('head2').style="color: #FFBF00;";
+    document.getElementById('para1').style="color: #D79334;";
+    document.getElementById('para2').style="color: #D79334;";
+    document.getElementById('footer').style="color: #D79334;";
+    document.getElementById('dark').style="background-color: #D79334;";
+    document.getElementById('light').style="background-color: #FFFFFF;";
+    document.getElementById('green').style="background-color: #FFFFFF;";
+
 }
 
 function LightMode() {
     document.getElementById('PageBody').style="background-color: #E9D5AC;";
-    document.getElementById('head1').style="color: #283859;";
-    document.getElementById('head2').style="color: #283859;";
+    document.getElementById('head1').style="color: #283895;";
+    document.getElementById('head2').style="color: #283895;";
     document.getElementById('para1').style="color: #854F3D;";
     document.getElementById('para2').style="color: #854F3D;";
     document.getElementById('footer').style="color: #854F3D;";
-    document.getElementById('break').style="color: #283859;";
+    document.getElementById('light').style="background-color: #854F3D;";
+    document.getElementById('dark').style="background-color: #FFFFFF;";
+    document.getElementById('green').style="background-color: #FFFFFF;";
 }
 
-function Default() {
-    document.getElementById('PageBody').style="background-color: #FFFFFF;";
+function GreenMode() {
+    document.getElementById('PageBody').style="background-color: #181C14;";
     document.getElementById('head1').style="color: #000000;";
     document.getElementById('head2').style="color: #000000;";
-    document.getElementById('para1').style="color: #000000;";
-    document.getElementById('para2').style="color: #000000;";
-    document.getElementById('footer').style="color: #000000;";
+    document.getElementById('para1').style="color: #EAE0D5;";
+    document.getElementById('para2').style="color: #EAE0D5;";
+    document.getElementById('footer').style="color: #EAE0D5;";
+    document.getElementById('light').style="background-color: #FFFFFF;";
+    document.getElementById('dark').style="background-color: #FFFFFF;";
+    document.getElementById('green').style="background-color: #FFFFFF;";
+
 }
