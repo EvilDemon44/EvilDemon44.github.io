c53e26791a61f8e093cd7194c34b41f7e8a04767
EvilDemon44 <alexanderkburns2318@gmail.com>
2025-01-22 11:49:02 -0700

    working on buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,19 +4,28 @@
 
 function DarkMode() {
     document.getElementById('PageBody').style="background-color: #363635;";
-
-
+    document.getElementById('head1').style="color: #FFFFFF;";
+    document.getElementById('head2').style="color: #FFFFFF;";
+    document.getElementById('para1').style="color: #B39C4D;";
+    document.getElementById('para2').style="color: #B39C4D;";
+    document.getElementById('footer').style="color: #EAE0D5;";
 }
 
 function LightMode() {
-    document.getElementById('PageBody').style="background-color: #FFFFFF;";
-    document.getElementsByClassName('dark').style="color #FFFFFF;";
-    document.getElementsByClassName('light').style.color="#FFFFFF;";
-
-
+    document.getElementById('PageBody').style="background-color: #E9D5AC;";
+    document.getElementById('head1').style="color: #283859;";
+    document.getElementById('head2').style="color: #283859;";
+    document.getElementById('para1').style="color: #854F3D;";
+    document.getElementById('para2').style="color: #854F3D;";
+    document.getElementById('footer').style="color: #854F3D;";
+    document.getElementById('break').style="color: #283859;";
 }
+
 function Default() {
-    document.getElementById('PageBody').style="background-color: #;";
-
-
+    document.getElementById('PageBody').style="background-color: #FFFFFF;";
+    document.getElementById('head1').style="color: #000000;";
+    document.getElementById('head2').style="color: #000000;";
+    document.getElementById('para1').style="color: #000000;";
+    document.getElementById('para2').style="color: #000000;";
+    document.getElementById('footer').style="color: #000000;";
 }
