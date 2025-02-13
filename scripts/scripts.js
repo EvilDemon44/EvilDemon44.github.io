55911c09458162cb1a3611de19cd13ea1b53f735
Alexander Burns <Alexanderkburns2318@gmail.com>
2025-02-13 06:22:40 +0000

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -43,22 +43,3 @@ function GreenMode() {
 
 }
 
-function Perimeter() {
-    let lengthP = parseInt(document.getElementById('Length1').value);
-    let widthP = parseInt(document.getElementById('Width1').value);
-    let resultP = 2 * (widthP + lengthP)
-    document.getElementById('Perimeter').innerHTML = resultP;
-}
-function Area() {
-    let lengthA = parseInt(document.getElementById('Length2').value);
-    let widthA = parseInt(document.getElementById('Width2').value);
-    let resultA = (widthA * lengthA)
-    document.getElementById('Area').innerHTML = resultA;
-}
-function Volume() {
-    let lengthV = parseInt(document.getElementById('Length3').value);
-    let widthV = parseInt(document.getElementById('Width3').value);
-    let heightV = parseInt(document.getElementById('Height3').value);
-    let resultV = lengthV * widthV * heightV
-    document.getElementById('Volume').innerHTML = resultV;
-}
